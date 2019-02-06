24947b0a576170524afad2002d3d991be0b59975
Komal Kumar <komal.teru@gmail.com>
2019-02-05 19:26:05 -0500

    Parameterized sampling for evaluation

--- evaluate.py
+++ evaluate.py
@@ -17,6 +17,8 @@ parser.add_argument("--p_norm", type=int, default=1,
                     help="The norm to use for the distance metric")
 parser.add_argument("--embedding_dim", type=int, default=50,
                     help="Entity and relations embedding size")
+parser.add_argument("--neg_sample_size", type=int, default=30,
+                    help="No. of negative samples to compare to for MRR/MR/Hit@10")
 
 params = parser.parse_args()
 
@@ -25,7 +27,7 @@ params.exp_dir = os.path.join(exps_dir, params.experiment_name)
 
 test_data_sampler = DataSampler(TEST_DATA_PATH)
 transE = initialize_model(params)
-evaluator = Evaluator(transE, test_data_sampler)
+evaluator = Evaluator(transE, test_data_sampler, params.neg_sample_size)
 
 logging.info('Testing model %s' % os.path.join(params.exp_dir, 'best_model.pth'))
 
